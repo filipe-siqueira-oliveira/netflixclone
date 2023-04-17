8224115d62de629673151d2dd41eaab7cf878c7d
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-04-17 08:43:49 -0300

    Responsive and Favicon

--- src/App.js
+++ src/App.js
@@ -60,6 +60,12 @@ export default () => {
       <footer>
         Developed by <strong>Filipe Siqueira de Oliveira</strong>
       </footer>
+
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='Carregando' />
+        </div>
+      }
     </div >
   )
 }
